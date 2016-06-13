b149678916be3a653b08f41c9ccf878a534f654e
Selvaraaju Murugesan <selvaraaju@gmail.com>
2016-06-13 09:53:49 +0530

    Add files via upload

---
+++ BLE_RGB_Lights_Send_Temp.ino
@@ -0,0 +1,151 @@
+// Author : Dr. Selvaraaju Murugesan
+// Date   : 7/6/2016
+
+#include <Adafruit_NeoPixel.h>
+
+int outputpin = A0;
+
+#define PIXEL_PIN    6    // Digital IO pin connected to the NeoPixels.
+
+#define PIXEL_COUNT 16
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+String readString;
+int showType = 0;
+
+void colorWipe(uint32_t, uint8_t);
+void setup() {
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+  Serial.begin(9600);
+}
+
+void loop() {
+  float rawvoltage = analogRead(outputpin);
+  float celsius = 5 * rawvoltage * 100 / 1024;
+  Serial.print(celsius);
+  delay(1000);
+
+  while (Serial.available()) {
+    delay(3);
+    char c = Serial.read();
+    readString += c;
+  }
+  if (readString.length() > 0) {
+    Serial.println(readString);
+    if (readString == "red")
+    {
+      colorWipe(strip.Color(255, 0, 0), 50); //red
+    }
+    if (readString == "green")
+    {
+      colorWipe(strip.Color(0, 255, 0), 50);  // Green
+    }
+    if (readString == "blue")
+    {
+      colorWipe(strip.Color(0, 0, 255), 50);  // Blue
+    }
+    if (readString == "magenta")
+    {
+      colorWipe(strip.Color(255, 0, 255), 50);  // Magenta
+    }
+    if (readString == "yellow")
+    {
+      colorWipe(strip.Color(255, 255, 0), 50);  // Yellow
+    }
+    if (readString == "orange")
+    {
+      colorWipe(strip.Color(255, 165, 0), 50);  // Orange
+    }
+    if (readString == "indigo")
+    {
+      colorWipe(strip.Color(75, 0, 130), 50);  // Indigo
+    }
+    readString = "";
+  }
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for (uint16_t i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for (j = 0; j < 256; j++) {
+    for (i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for (j = 0; j < 256 * 5; j++) { // 5 cycles of all colors on wheel
+    for (i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j = 0; j < 10; j++) { //do 10 cycles of chasing
+    for (int q = 0; q < 3; q++) {
+      for (int i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, c);  //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (int i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, 0);      //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j = 0; j < 256; j++) {   // cycle all 256 colors in the wheel
+    for (int q = 0; q < 3; q++) {
+      for (int i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, Wheel( (i + j) % 255)); //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (int i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, 0);      //turn every third pixel off
+      }
+    }
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if (WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
